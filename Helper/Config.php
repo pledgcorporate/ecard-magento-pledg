76646edfe17a8a4c7ebaddc7853410e7c98b61aa
jmarichez <julien.marichez1@gmail.com>
2023-08-31 16:53:06 +0000

    Build: (b6c0a2c) update changelog for 1.2.4 and 1.2.5

--- Helper/Config.php
+++ Helper/Config.php
@@ -9,7 +9,7 @@ use Magento\Sales\Model\Order;
 
 class Config extends AbstractHelper
 {
-    const MODULE_VERSION = '1.2.3';
+    const MODULE_VERSION = '1.2.5';
     const ORDER_REFERENCE_PREFIX = 'order_';
 
     /**
